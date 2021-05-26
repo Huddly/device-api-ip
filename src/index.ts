efc6e384047a15633213593b5b04448c87c833ce
Brikend <brikend@huddly.com>
2021-05-26 10:03:49 +0200

    feat: finish draft implementation of index.ts and traansport

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import IHuddlyDeviceAPI from '@huddly/sdk/lib/src/interfaces/iHuddlyDeviceAPI';
 import { EventEmitter } from 'events';
 import IUVCControlAPI from '@huddly/sdk/lib/src/interfaces/iUVCControlApi';
-import ITransport from '@huddly/sdk/lib/src/interfaces/iTransport';
 import IDeviceDiscovery from '@huddly/sdk/lib/src/interfaces/iDeviceDiscovery';
 import DeviceApiOpts from '@huddly/sdk/lib/src/interfaces/IDeviceApiOpts';
 import DeviceDiscoveryManager from './manager';
 import Logger from '@huddly/sdk/lib/src/utilitis/logger';
+import HuddlyDevice from './networkdevice';
+import GrpcTransport from './transport';
+import IGrpcTransport from '@huddly/sdk/lib/src/interfaces/IGrpcTransport';
 
-export default class HuddlyDeviceAPIUSB implements IHuddlyDeviceAPI {
+export default class HuddlyDeviceApiIP implements IHuddlyDeviceAPI {
     logger: any;
     eventEmitter: EventEmitter;
     deviceDiscoveryManager: DeviceDiscoveryManager;
     maxSearchRetries: Number;
     alwaysRetry: boolean;
+
+    private readonly SUPPORTED_DEVICES: String[] = ['L1'];
 
     constructor(opts: DeviceApiOpts = {}) {
         this.logger = opts.logger || new Logger(true);
@@ -32,13 +36,26 @@ export default class HuddlyDeviceAPIUSB implements IHuddlyDeviceAPI {
         return this.deviceDiscoveryManager;
     }
 
-    async getValidatedTransport(device): Promise<ITransport> {
-        throw new Error('Not supported yet');
-        // TODO
+    async getValidatedTransport(device: HuddlyDevice): Promise<IGrpcTransport> {
+        if (device && (device.manufacturer != 'Huddly' || !this.SUPPORTED_DEVICES.includes(device.name))) {
+            this.logger.warn(`There is no supported transport implementation for device ${device.name} from manufacturer ${device.manufacturer}`, HuddlyDeviceApiIP.name);
+            return undefined;
+        }
+
+        try {
+            const transport = await this.getTransport(device);
+            // TODO: some sort of handshake ?
+            this.logger.info('Transport protocol is GRPC', HuddlyDeviceApiIP.name);
+            return transport;
+        } catch (e) { // TODO: catch specific exception
+            this.logger.error(`GRPC Transport implementation not supported for device ${device.toString()}`, e, HuddlyDeviceApiIP.name);
+        }
     }
 
-    async getTransport(device): Promise<any> {
-        throw new Error('Not supported yet');
+    async getTransport(device: HuddlyDevice): Promise<IGrpcTransport> {
+        const transport = new GrpcTransport(device, this.logger);
+        await transport.init();
+        return transport;
     }
 
     async isUVCControlsSupported(_: any) {
