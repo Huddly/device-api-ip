17dedfa73861e75bb981223012ac22410e0e695c
Brikend <brikend@huddly.com>
2021-05-26 16:21:34 +0200

    DONT KEEP: update test.js script

--- test.js
+++ test.js
@@ -1,32 +1,36 @@
-var Manager = require('./lib/manager').default;
+var Api = require('./lib/src/index').default;
 const EventEmitter = require('events');
 
 const main = async () => {
 
-    var manager = new Manager();
-
+    var api = new Api();
     class MyEmitter extends EventEmitter {}
     const myEmitter = new MyEmitter();
-    manager.registerForHotplugEvents(myEmitter);
+    let grpcTransport;
+    let mydevice;
 
-    /*
-    // List devices example
 
-    const list = await manager.deviceList();
-    console.log(`List contains ${list.length} devices `);
-    list.forEach((d) => {
-        console.log(d.toString());
-    })
-    */
-
-    /*
-    // Get device example
-    manager.getDevice('12101A0026').then((device) => {
-        console.log("Device with serial 12101A0026 found")
-    }).catch((reason) => {
-        console.log(reason);
+    myEmitter.on('ATTACH', (device) => {
+        mydevice = device;
+        api.getValidatedTransport(device)
+        .then((transport) => {
+            grpcTransport = transport;
+            grpcClient = transport.grpcClient;
+            // TODO: Perform actions
+            console.log('We can perform actions on the device now');
+            console.log(mydevice);
+        }).catch((e) => {
+            console.log('Unable to get transport for device');
+            console.error(e);
+        })
     });
-    */
+    myEmitter.on('DETACH', (device) => {
+        if (mydevice && mydevice.equals(device)) {
+            console.log('Closing grpc channel on device');
+            grpcTransport.close();
+        }
+    });
+    api.registerForHotplugEvents(myEmitter);
 }
 
 main();
